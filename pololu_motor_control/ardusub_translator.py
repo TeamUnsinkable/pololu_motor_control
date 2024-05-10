ff2c1f517951757af351f3e05a9825f47192b0ea
Sirio Jansen-Sanchez <siriojansen@hotmail.com>
2024-05-10 00:24:50 -0400

    Reworked translator

--- pololu_motor_control/ardusub_translator.py
+++ pololu_motor_control/ardusub_translator.py
@@ -1,76 +1,128 @@
 import rclpy
 from rclpy.node import Node
+from rclpy.callback_groups import MutuallyExclusiveCallbackGroup, ReentrantCallbackGroup
+from rclpy.executors import MultiThreadedExecutor
+from collections import deque
 
 from std_msgs.msg import Int16, Float32
 
 class ArudoSubTranslator(Node):
     def __init__(self):
-        super().__init__("ArduSubTranslator")
-        self.motor_1_pub = self.create_publisher(Int16, "/output/motor1", 5)
-        self.motor_2_pub = self.create_publisher(Int16, "/output/motor2", 5)
-        self.motor_3_pub = self.create_publisher(Int16, "/output/motor3", 5)
-        self.motor_4_pub = self.create_publisher(Int16, "/output/motor4", 5)
-        self.motor_5_pub = self.create_publisher(Int16, "/output/motor5", 5)
-        self.motor_6_pub = self.create_publisher(Int16, "/output/motor6", 5)
-        self.motor_7_pub = self.create_publisher(Int16, "/output/motor7", 5)
-        self.motor_8_pub = self.create_publisher(Int16, "/output/motor8", 5)
+        super().__init__("ArduSubTranslator") # type: ignore
+        self.sub_cbg = ReentrantCallbackGroup()
+        self.timer_cbg = MutuallyExclusiveCallbackGroup()
 
-        self.create_subscription(Float32, "/controls/yaw/hotas", self.yaw_callback, 5)
-        self.create_subscription(Float32, "/controls/surge/hotas", self.surge_callback, 5)
-        self.create_subscription(Float32, "/controls/sway/hotas", self.sway_callback, 5)
-        self.create_subscription(Float32, "/controls/depth/hotas", self.depth_callback, 5)
+        self.declare_parameter("rate", 12)
+        self.motor_publishers = [
+            self.create_publisher(Int16, "/output/motor1", 10),
+            self.create_publisher(Int16, "/output/motor2", 10),
+            self.create_publisher(Int16, "/output/motor3", 10),
+            self.create_publisher(Int16, "/output/motor4", 10),
+            self.create_publisher(Int16, "/output/motor5", 10),
+            self.create_publisher(Int16, "/output/motor6", 10),
+            self.create_publisher(Int16, "/output/motor7", 10),
+            self.create_publisher(Int16, "/output/motor8", 10),
+        ]
+
+        # Indexes are {0: Yaw, 1: Surge, 2: Sway}
+        self.motor_values = [deque()]*8
+
+        self.create_timer(1/self.get_parameter("rate").get_parameter_value().integer_value, self.timer_callback, callback_group=self.timer_cbg)
+        self.create_subscription(Float32, "/controls/yaw/hotas", self.yaw_callback, 10, callback_group=self.sub_cbg)
+        self.create_subscription(Float32, "/controls/surge/hotas", self.surge_callback, 10, callback_group=self.sub_cbg)
+        self.create_subscription(Float32, "/controls/sway/hotas", self.sway_callback, 10, callback_group=self.sub_cbg)
+        self.create_subscription(Float32, "/controls/depth/hotas", self.depth_callback, 10, callback_group=self.sub_cbg)
         self.get_logger().info("Doing the dishes")
 
     def _base_pwm_conversion(self, number):
-        num = Int16()
+        num = round(number)
         # Negative Limit Checking
-        calculation = max(number+1500, 1100)
+        calculation = max(round(num), -400)
         # Positive Limit Checking
-        calculation = min(calculation, 1900)
-        num.data = int(round(calculation))
+        calculation = min(round(calculation), 400)
         return num
+    
+    def timer_callback(self) -> None:
+        for idx, queue in enumerate(self.motor_values):
+            self.get_logger().info(f"Processing PWM for Motor: {idx+1}")
+            # Create Empty Message
+            msg = Int16()
+            # Setup Average Parameters
+            cnt = len(queue)
+            tot = 0
+            # Try for exepcted amount but be ready for failures
+            try:
+                for _ in range(len(queue)):
+                    tot += queue.popleft()
+            except IndexError:
+                # No more values in array
+                self.get_logger().info(f"Motors IDX: {idx+1} ran into issues computing PWM")
+                break
+            avg = tot/cnt
+            if avg == 0:
+                msg.data = 1500
+            else:
+                msg.data = self._base_pwm_conversion(avg)+1500
+
+            if round(msg.data) == -1500:
+                return
+            
+            self.motor_publishers[idx].publish(msg)
+
+
 
     def yaw_callback(self, msg: Float32):
         num = self._base_pwm_conversion(msg.data)
-        
-        self.motor_2_pub.publish(num)
-        self.motor_4_pub.publish(num)
+        # Motor 2 and 3 Foward
+        self.motor_values[1].append(num)
+        self.motor_values[2].append(num)
 
-        num.data *= -1
-        self.motor_1_pub.publish(num)
-        self.motor_3_pub.publish(num)
+        # Motor 1 and 4 Inversed
+        self.motor_values[0].append(-num)
+        self.motor_values[3].append(-num)
+
         
     def surge_callback(self, msg: Float32):
         num = self._base_pwm_conversion(msg.data)
-        self.motor_2_pub.publish(num)
-        self.motor_1_pub.publish(num)
+        # Motor 1 and 2
+        self.motor_values[0].append(num)
+        self.motor_values[1].append(num)
         
-        num.data *= -1
-        self.motor_4_pub.publish(num)
-        self.motor_3_pub.publish(num)
+        # Motor 3 and 4 Inversed
+        self.motor_values[2].append(-num)
+        self.motor_values[3].append(-num)
+
+
 
     def sway_callback(self, msg: Float32):
         num = self._base_pwm_conversion(msg.data)
-        self.motor_2_pub.publish(num)
-        self.motor_1_pub.publish(num)
+        # Motor 2 and 4
+        self.motor_values[1].append(num)
+        self.motor_values[3].append(num)
         
-        num.data *= -1
-        self.motor_4_pub.publish(num)
-        self.motor_3_pub.publish(num)
-        
+        # Motor 1 and 3 Inversed
+        self.motor_values[0].append(-num)
+        self.motor_values[2].append(-num)
+
+     
 
     def pitch_callback(self, msg: Float32):
         pass
 
     def depth_callback(self, msg: Float32):
         num = self._base_pwm_conversion(msg.data)
-        self.motor_2_pub.publish(num)
-        self.motor_1_pub.publish(num)
-        self.motor_4_pub.publish(num)
-        self.motor_3_pub.publish(num)
+        # Motor 2 and 4
+        self.motor_values[4][3] += num.data
+        self.motor_values[5][3] += num.data
+        self.motor_values[6][3] += num.data
+        self.motor_values[7][3] += num.data
 
 def main():
     rclpy.init()
+    executor = MultiThreadedExecutor()
     ardu_boi = ArudoSubTranslator()
-    rclpy.spin(ardu_boi)
-    rclpy.shutdown()
+    executor.add_node(ardu_boi)
+    try:
+        executor.spin()
+    finally:
+        rclpy.shutdown()
