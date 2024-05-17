786db30ad3e989e346a869bd58e1e6f846a06f98
UnsinkableERAU <UnsinkableERAU@gmail.com>
2024-05-17 13:21:59 -0400

    Modified thrust mapping

--- pololu_motor_control/node.py
+++ pololu_motor_control/node.py
@@ -6,20 +6,19 @@ from std_msgs.msg import Int16, Bool
 from std_srvs.srv import SetBool
 from threading import Lock
 from time import sleep
-
 from amra_utils_py.helpers import generate_header
 
 # Converts ardusub thruster mappings to pololu
 # ardusub: pololu
 motor_mapping = {
-    1: 1,
-    2: 2,
-    3: 3,
-    4: 4,
-    5: 5,
-    6: 6,
-    7: 7,
-    8: 8
+    1: 8,
+    2: 1,
+    3: 2,
+    4: 3,
+    5: 7,
+    6: 5,
+    7: 4,
+    8: 6
 }
 
 class MaestroWritter(Node):
